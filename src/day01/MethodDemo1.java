e6432b1f735c54907e5a273d1bd880f47142e461
DESKTOP-57S7PKJ\Lulu <waleilulu@gmail.com>
2024-10-18 02:07:13 +0800

    Add new files

---
+++ src/day01/MethodDemo1.java
@@ -0,0 +1,14 @@
+package day01;
+
+// 方法的調用 1
+public class MethodDemo1 {
+    public static void main(String[] args) {
+		printWords();
+		printWords();
+	}
+	
+	public static void printWords() {
+		System.out.println("she sell sea shell on the sea shore");
+		
+	} 
+}
